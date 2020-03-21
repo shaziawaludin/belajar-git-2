54bbb35ae0144783408dc1b21f48e99f8ee761c4
Shazi Awaludin <shaziawaludin23@gmail.com>
2020-03-22 04:07:23 +0700

    menambahkan lorem

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hey hey</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis ullam, culpa facere, distinctio pariatur doloremque id ratione vel nulla rem eum sed corrupti esse provident eaque cumque saepe ex? Debitis!</p>
 </body>
 </html>
